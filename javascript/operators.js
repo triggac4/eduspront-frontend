414c25b496fdb8af7b3b456af7f7b752a84d5fc1
Kenechukwu Offokansi <koffokansi@gmail.com>
2024-07-24 23:14:06 +0100

    javascript

---
+++ javascript/operators.js
@@ -0,0 +1,32 @@
+//Arithmetic Operators
+//Comparison Operators
+//Logical Operators
+
+//Arithmetic Operators
+const a = "8";
+const b = 8;
+const c = 10;
+
+const d = 10 - 3;
+// + - / * % **
+
+// console.log(d);
+
+//comparison operators return boolean
+// > < >= <= == === != !==
+const e = b > a;
+const f = b < a;
+const g = b >= a;
+const h = b <= a;
+const i = b == a;
+const j = b === a;
+const k = b != a;
+const l = b !== a;
+// console.log(e);
+// console.log(k);
+
+// logical operators
+// || && !
+
+const m = !false;
+console.log(m);
---
+++ javascript/variables.js
@@ -0,0 +1,27 @@
+const a = 5;
+let b = 8;
+var c = 10;
+
+//variable names and the rules of naming a variable
+
+//1. variable name should not start with numbers but can contain a number
+//2. variable name should not contain any special characters except _ $
+//3. variable name should not contain any space
+console.log(b);
+// snake case
+const lecture_two = 5;
+
+//camel case
+const lectureTwo = 5;
+
+//pascal case
+const LectureTwo = 5;
+
+var samuel = 25;
+
+samuel = 27;
+b = 78;
+
+console.log(b);
+
+
